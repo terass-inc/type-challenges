3f59aa43148932b05dd015b8f098291e6bcd7d2b
kkb <kkb@kkbs-MacBook-Pro.local>
2023-04-28 17:14:24 +0900

    result 8 & prepare 9

--- work/8/04425-medium-greater-than.ts
+++ work/8/04425-medium-greater-than.ts
@@ -23,6 +23,210 @@
   > GitHubで確認する：https://tsch.js.org/4425/ja
 */
 
+// Kosaka
+type ParseInt<T> = T extends `${infer Digit extends number}` ? Digit : never
+
+type NumberToTuple<
+  T extends number,
+  _T extends string = `${T}`,
+  __T extends number[] = []
+> = _T extends `${infer H}${infer R}`
+  ? NumberToTuple<never, R, [...__T, ParseInt<H>]>
+  : __T
+
+type CreateTuple<
+  T extends number,
+  R extends number[] = []
+> = R["length"] extends T ? R : CreateTuple<number, [1, ...R]>
+
+type GreaterThanDigit<
+  T extends number,
+  U extends number,
+  S extends number[] = CreateTuple<T>
+> = S["length"] extends 10
+  ? true
+  : S["length"] extends U
+  ? false
+  : GreaterThanDigit<T, U, [1, ...S]>
+
+type GreaterThan<
+  T extends number,
+  U extends number,
+  _T extends number[] = NumberToTuple<T>,
+  _U extends number[] = NumberToTuple<U>
+> = _T["length"] extends _U["length"]
+  ? _T extends [infer _TF, ...infer _TR]
+    ? _U extends [infer _UF, ...infer _UR]
+      ? false
+      : false
+    : false
+  : false
+
+
+// 最後のケースが通らない
+// タプルV(初期値は[]）に要素を追加していく
+// VのlengthがTに達した時点でfalse, その前にUに達したらtrue
+type EchizenGreaterThan<T extends number, U extends number, V extends any[] = []> = V['length'] extends T
+  ? false
+  : V['length'] extends U
+  ? true
+  : EchizenGreaterThan<T, U, [true, ...V]>
+
+
+
+// hayashi 究極のMinus One使った
+type ParseInt<T extends string> = T extends `${infer Digit extends number}` ? Digit : never
+type ReverseString<S extends string> = S extends `${infer First}${infer Rest}` ? `${ReverseString<Rest>}${First}` : ''
+type RemoveLeadingZeros<S extends string> = S extends '0' ? S : S extends `${'0'}${infer R}` ? RemoveLeadingZeros<R> : S
+type InternalMinusOne<
+  S extends string
+> = S extends `${infer Digit extends number}${infer Rest}` ?
+    Digit extends 0 ?
+      `9${InternalMinusOne<Rest>}` :
+    `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}`:
+  never
+
+type MinusOne<T extends number> = ParseInt<RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>>
+
+type GreaterThan<T extends number, U extends number> = T extends 0
+  ? false
+  : MinusOne<T> extends U
+    ? true
+    : GreaterThan<MinusOne<T>, U>
+
+
+// kkb
+type StringToNumber<T extends string> =
+  T extends '0' ? 0 :
+  T extends '1' ? 1 :
+  T extends '2' ? 2 :
+  T extends '3' ? 3 :
+  T extends '4' ? 4 :
+  T extends '5' ? 5 :
+  T extends '6' ? 6 :
+  T extends '7' ? 7 :
+  T extends '8' ? 8 :
+  T extends '9' ? 9 :
+  never
+
+type ToArray<T extends number | string> = T extends string
+  ? T extends `${infer Head}${infer Tail}`
+    ? [StringToNumber<Head>, ...ToArray<Tail>]
+    : []
+  : ToArray<`${T}`>
+
+type ToUnknownArray<T extends number, U extends unknown[] = []> = T extends U['length']
+  ? U
+  : ToUnknownArray<T, [unknown, ...U]>
+
+type GreaterThanUnknownArray<T extends unknown[], U extends unknown[]> = T extends U
+  ? false
+  : T extends [infer _, ...infer TT]
+    ? U extends [infer _, ...infer UT]
+      ? GreaterThanUnknownArray<TT, UT>
+      : true
+    : false
+
+type hoge = GreaterThanUnknownArray<ToUnknownArray<4>, ToUnknownArray<4>>
+
+type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+type GreaterThan<
+  T extends number,
+  U extends number,
+  _T extends any[] = ToArray<T>,
+  _U extends any[] = ToArray<U>,
+> = _T['length'] extends _U['length']
+  ? _T extends [infer TH, ...infer TT]
+    ? _U extends [infer UH, ...infer UT]
+      ? TH extends UH
+        ? GreaterThan<T, T, TT, UT>
+        : TH extends ZeroToNine
+          ? UH extends ZeroToNine
+            ? GreaterThanUnknownArray<ToUnknownArray<TH>, ToUnknownArray<UH>>
+            : never
+          : never
+      : true
+    : false
+  : _T extends [infer _, ...infer TT]
+    ? _U extends [infer _, ...infer UT]
+      ? GreaterThan<T, U, TT, UT>
+      : true
+    : false
+
+
+
+// いわせ(ちょっとこたえみた)
+type iwaseGreaterThan<T extends number, U extends number> = ((
+  ...args: [...Array<T>]
+) => unknown) extends (x: unknown, ...args: [...Array<U>]) => unknown
+  ? false
+  : true
+
+
+
+
+
+//wada
+type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+
+// type CompareConverter = {
+//   '9': '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8'
+//   '8': '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7'
+//   '7': '0' | '1' | '2' | '3' | '4' | '5' | '6'
+//   '6': '0' | '1' | '2' | '3' | '4' | '5'
+//   '5': '0' | '1' | '2' | '3' | '4'
+//   '4': '0' | '1' | '2' | '3'
+//   '3': '0' | '1' | '2'
+//   '2': '0' | '1'
+//   '1': '0'
+//   '0': never
+// }
+// type CompareLeftDigit<
+//   T extends Digit,
+//   U extends Digit,
+// > = U extends CompareConverter[T] ? true : false
+
+type CompareLeftDigit<
+  T extends Digit,
+  U extends Digit,
+  V extends any[] = [],
+> = `${V['length']}` extends T
+  ? false
+  : `${V['length']}` extends U
+  ? true
+  : CompareLeftDigit<T, U, [true, ...V]>
+
+type Compare<
+  T extends string, // MUST be same legnth
+  U extends string, // MUST be same legnth
+> = T extends `${infer FT extends Digit}${infer RT}`
+  ? U extends `${infer FU extends Digit}${infer RU}`
+    ? RT extends ''
+      ? CompareLeftDigit<FT, FU>
+      : FT extends FU
+      ? Compare<RT, RU>
+      : CompareLeftDigit<FT, FU>
+    : never
+  : never
+  
+type IsLonger<
+  T extends string,
+  U extends string,
+> = T extends `${infer _}${infer RT}`
+  ? U extends `${infer _}${infer RU}`
+    ? IsLonger<RT, RU>
+    : true
+  : U extends `${infer _}${infer _}`
+  ? false
+  : 'same'
+
+type wadaGreaterThan<
+  T extends number,
+  U extends number,
+  Z = IsLonger<`${T}`, `${U}`>,
+> = Z extends 'same' ? Compare<`${T}`, `${U}`> : Z
+//-wada
 /* _____________ ここにコードを記入 _____________ */
 
 type GreaterThan<T extends number, U extends number> = any
--- work/8/04471-medium-zip.ts
+++ work/8/04471-medium-zip.ts
@@ -15,8 +15,63 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type Zip<T, U> = any
+type kosakaZip<T extends any[], U extends any[]> = T extends [infer A, ...infer B]
+  ? U extends [infer C, ...infer D]
+    ? [[A, C], ...kosakaZip<B, D>]
+    : []
+  : []
 
+type echizenZip<T, U> = [T, U] extends [[infer TF, ...infer TR], [infer UF, ...infer UR]]
+ ? [[TF, UF], ...echizenZip<TR, UR>] 
+ : []
+
+
+type koikeZip<T, U> = [T, U] extends [
+    [infer THead, ...infer TRest],
+    [infer UHead, ...infer URest]
+  ]
+    ? [[THead, UHead], ...Zip<TRest, URest>]
+    : []
+    
+    
+// kkb
+type kkbZip<T extends any[], U extends any[]> = T extends [infer TH, ...infer TT]
+  ? U extends [infer UH, ...infer UT]
+    ? [[TH, UH], ...kkbZip<TT, UT>]
+    : []
+  : []
+
+// hayashi
+type hysZip<T extends any[], U extends any[]> = T extends [infer F, ...infer L]
+  ? U extends [infer T, ...infer K]
+    ? [[F, T], ...hysZip<L, K>]
+    : []
+  : []
+
+//wada
+type wadaZip<
+  T extends any[],
+  U extends any[],
+  V extends any[] = [],
+> = V['length'] extends T['length']
+  ? V
+  : V['length'] extends U['length']
+  ? V
+  : wadaZip<T, U, [...V, [T[V['length']], U[V['length']]]]>
+
+  // nakano
+  type Zip<T extends any[], U extends any[]> = Filter<{
+    [A in keyof T]: [T[A], A extends keyof U ? U[A] : never]
+  }>
+  type Filter<T extends any[]> = T extends [
+    infer H,
+    ...infer T extends [any, any][]
+  ]
+    ? H extends [any, never]
+      ? Filter<T>
+      : [H, ...Filter<T>]
+    : []
+  
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
